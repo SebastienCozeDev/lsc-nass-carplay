b8eadc7a7d538e6c4b12f81abffeb044c0d7684f
Sébastien Coze <96086580+SebastienCozeDev@users.noreply.github.com>
2024-09-23 10:47:07 +0000

    Add the french translation

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,15 +3,16 @@ use_experimental_fxv2_oal 'yes'
 game 'gta5'
 lua54 'yes'
 
+name 'lsc-nass-carplay'
 description 'nass_carplay'
-author 'Nass#1411'
-version '1.1.0'
+author 'Nass#1411 & SebastienCozeDev'
+version '1.2.0'
 
 server_scripts { 'server/*.lua' }
 
 client_scripts { 'client/en/*.lua' }
 
-ui_page 'html/en/index.html'
+ui_page 'html/en/index.html' -- Replace en by other language if you want. Available language: en, fr
 files { 
 	"html/*",
 }
