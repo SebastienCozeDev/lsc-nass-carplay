65db8d073fe148b3a0e4db48dbfbaf289ee6d4b2
Sébastien Coze <96086580+SebastienCozeDev@users.noreply.github.com>
2024-09-28 11:22:02 +0000

    Fix bug [test]

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,15 +4,15 @@ game 'gta5'
 lua54 'yes'
 
 name 'lsc-nass-carplay'
-description 'nass_carplay'
+description 'Nass Carplay for LSC Server.'
 author 'Nass#1411 & SebastienCozeDev'
 version '1.2.0'
 
 server_scripts { 'server/*.lua' }
 
-client_scripts { 'client/en/*.lua' }
+client_scripts { 'client/*.lua' }
 
-ui_page 'html/en/index.html' -- Replace en by other language if you want. Available language: en, es, fr
+ui_page 'html/index.html'
 files { 
 	"html/*",
 }
